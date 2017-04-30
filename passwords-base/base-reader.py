a81cbb400fcf5f56dfde0741e9652fd3400bbd48
pamepeixinho <pameiupip@gmail.com>
2017-04-29 23:05:54 -0300

    update caesar cript

--- passwords-base/base-reader.py
+++ passwords-base/base-reader.py
@@ -1,5 +1,7 @@
 import csv
-import string
+
+# test caesar http://www.dcode.fr/caesar-cipher
+ceaser_shift = 12
 
 def read_password_base():
     base_file_path = 'base.txt'
@@ -8,32 +10,38 @@ def read_password_base():
         base_reader = csv.reader(csvfile, delimiter='|')
         return list(base_reader)
 
-def ceaser_decrypt(message, shift):
-    message = message.lower()
-
+def caesar(password, shift):
+    password = password.lower()
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     secret = ""
-    for c in message:
-        if c in "abcdefghijklmnopqrstuvwxyz":
+    for c in password:
+        if c in alphabet:
             num = ord(c)
             num += shift
             if num > ord("z"):  # wrap if necessary
                 num -= 26
             elif num < ord("a"):
                 num += 26
-            secret = secret + chr(num)
+            secret += chr(num)
         else:
             # don't modify any non-letters in the message; just add them as-is
             secret = secret + c
-    return secret
+    return secret.upper()
 
 
-def decrypt_base_password(base):
+def caesar_decrypt(password):
+    return caesar(password, -ceaser_shift)
+
+
+def caesar_encrypt(password):
+    return caesar(password, ceaser_shift)
+
+
+def decrypt_base_password(password_base):
     print "decrypt"
-    for line in base:
-        for line in base:
-            print "before %s" % line[1]
-            print "after %s" % ceaser_decrypt(line[1], -12)
-
+    for line in password_base:
+        print "before %s" % line[1]
+        print "after %s" % caesar_decrypt(line[1])
 
 base = read_password_base()
 decrypt_base_password(base)
