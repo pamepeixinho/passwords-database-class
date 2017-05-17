c0d04ec1d0a3997b849e351a6172bcf4bda864b6
pamepeixinho <pameiupip@gmail.com>
2017-05-17 00:53:45 -0300

    count time to valid in each hash method

--- validateUser.py
+++ validateUser.py
@@ -1,5 +1,7 @@
 from PasswordBase.base_reader import read_password_base
-from PasswordConvertion.convertion import encrypt_to_sha256, encrypt_to_sha1, encrypt_to_md5
+from PasswordConvertion.convertion import encrypt_to_sha256, encrypt_to_sha1, encrypt_to_md5, caesar_encrypt
+
+import time
 
 base_file_path = './PasswordBase/base.txt'
 base_md5_file_path = './PasswordBase/base_md5.txt'
@@ -8,18 +10,39 @@ base_sha256_file_path = './PasswordBase/base_sha256.txt'
 
 
 def validate_user_md5(username, password):
+    start_time = time.time()
     digest = encrypt_to_md5(password)
-    return validate_user(username, digest, base_md5_file_path)
+    valid = validate_user(username, digest, base_md5_file_path)
+    time_used = (time.time() - start_time)
+    print("time_used MD5 --- %s seconds ---" % time_used)
+    return valid
 
 
 def validate_user_sha1(username, password):
+    start_time = time.time()
     digest = encrypt_to_sha1(password)
-    return validate_user(username, digest, base_sha1_file_path)
+    valid = validate_user(username, digest, base_sha1_file_path)
+    time_used = (time.time() - start_time)
+    print("time_used SHA1 --- %s seconds ---" % time_used)
+    return valid
 
 
 def validate_user_sha256(username, password):
+    start_time = time.time()
     digest = encrypt_to_sha256(password)
-    return validate_user(username, digest, base_sha256_file_path)
+    valid = validate_user(username, digest, base_sha256_file_path)
+    time_used = (time.time() - start_time)
+    print("time_used SHA256 --- %s seconds ---" % time_used)
+    return valid
+
+
+def validate_user_caesar(username, password):
+    start_time = time.time()
+    digest = caesar_encrypt(password)
+    valid = validate_user(username, digest, base_file_path)
+    time_used = (time.time() - start_time)
+    print("time_used CEASER --- %s seconds ---" % time_used)
+    return valid
 
 
 def validate_user(username, digest, base_file):
@@ -32,5 +55,7 @@ def validate_user(username, digest, base_file):
     return False
 
 
-print validate_user_md5('Denis.Ozires.Quelip', 'RIUPLKHD')
-print validate_user_md5('Rogerio.Bandit.Santos', 'KUKNadNFEJ')
+print validate_user_caesar('Rogerio.Bandit.Santos', 'KUKNANFEJ')
+print validate_user_md5('Rogerio.Bandit.Santos', 'KUKNANFEJ')
+print validate_user_sha1('Rogerio.Bandit.Santos', 'KUKNANFEJ')
+print validate_user_sha256('Rogerio.Bandit.Santos', 'KUKNANFEJ')
