529117245eb9ae78748aa53151fa831ace4206b1
pamepeixinho <pameiupip@gmail.com>
2017-06-04 12:47:52 -0300

    updating

--- report.py
+++ report.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from PasswordBase.base_reader import decrypt_base_password, read_password_base
 from PasswordBase.base_writer import writeNewBase
 from PasswordConvertion.convertion import encrypt_to_sha256, encrypt_to_sha1, encrypt_to_md5
@@ -8,74 +9,63 @@ import matplotlib.pyplot as plt
 base_file_path = './PasswordBase/'
 
 
-def countTime(hashName, converstionFunc):
+def count_time(hash_name, converstion_func):
     start_time = time.time()
     base_list = read_password_base(base_file_path + 'base.txt')
     list = decrypt_base_password(base_list)
-    convert_list(list, converstionFunc, hashName)
+    convert_list(list, converstion_func, hash_name)
     timeUsed = (time.time() - start_time)
-    # print("TIMEUSED %s --- %s seconds ---" % (hashName, timeUsed))
     return timeUsed
 
+times = 50
 
-def convert_list(list, convertionFunc, hashName):
+def convert_list(list, convertion_func, hash_hame):
     for line in list:
-        line[1] = convertionFunc(line[1])
-    writeNewBase(base_file_path + 'base_' + hashName + '.txt', list)
+        line[1] = convertion_func(line[1])
+    writeNewBase(base_file_path + 'base_' + hash_hame + '.txt', list)
 
 
 def reportMD5():
     md5Times = []
-    for x in range(0, 10):
-        md5Times.append(countTime('md5', encrypt_to_md5))
+    for x in range(0, times):
+        md5Times.append(count_time('md5', encrypt_to_md5))
 
     report('MD5', md5Times)
-    # print md5Times
-    # mean = np.average(md5Times)
-    # print("Mean %s --- %s seconds ---" % ('md5', mean))
     return md5Times
 
 
 def reportSHA1():
     sha1Times = []
-    for x in range(0, 10):
-        sha1Times.append(countTime('sha1', encrypt_to_sha1))
+    for x in range(0, times):
+        sha1Times.append(count_time('sha1', encrypt_to_sha1))
 
     report('SHA1', sha1Times)
-    # print md5Times
-    # mean = np.average(sha1Times)
-    # print mean
-    # print("Mean %s --- %s seconds ---" % ('sha1', mean))
     return sha1Times
 
 
 def reportSHA256():
     sha256Times = []
-    for x in range(0, 10):
-        sha256Times.append(countTime('sha256', encrypt_to_sha256))
+    for x in range(0, times):
+        sha256Times.append(count_time('sha256', encrypt_to_sha256))
 
     report('SHA256', sha256Times)
     return sha256Times
 
 
 def report(name, array):
-    mean = np.average(array)
-    min = np.amin(array)
-    max = np.amax(array)
     var = np.var(array)
-    dp = np.sqrt(var)
 
-    print("Mean %s --- %s seconds ---" % (name, mean))
-    print("Min %s --- %s seconds ---" % (name, min))
-    print("Max %s --- %s seconds ---" % (name, max))
+    print("Mean %s --- %s seconds ---" % (name, np.average(array)))
+    print("Min %s --- %s seconds ---" % (name, np.amin(array)))
+    print("Max %s --- %s seconds ---" % (name, np.amax(array)))
     print("Var %s --- %s seconds ---" % (name, var))
-    print("DP %s --- %s seconds ---" % (name, dp))
+    print("DP %s --- %s seconds ---" % (name, np.sqrt(var)))
 
 
 def ploting(x1, x2, x3):
-    plt.plot(x1, 'r--',  marker='o', label='MD5')
-    plt.plot(x2, 'b--',  marker='o', label='SHA1')
-    plt.plot(x3, 'g--',  marker='o', label='SHA256')
+    plt.plot(x1, 'r--', label='MD5')
+    plt.plot(x2, 'b--', label='SHA1')
+    plt.plot(x3, 'g--',  label='SHA256')
 
     plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
                ncol=2, mode="expand", borderaxespad=0.)
@@ -90,5 +80,3 @@ x2 = reportSHA1()
 x3 = reportSHA256()
 
 ploting(x1, x2, x3)
-
-# http://stackoverflow.com/questions/20412038/printing-python-code-to-pdf OLHAR
